df0f12e63a7c1a364b62089891888edc708d4210
lihongjing <tolihongjing@gmail.com>
2014-12-28 17:42:00 +0800

    angular + browserify

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,24 @@
 (function() {
-	// 'use strict';
-  //       require('angular');
-  //       require('angular-route');
-  //       require('angular-animate');
-  //       var mainCtrl = require('./controllers/mainctrl');
-	// angular.module('SampleApp', ['ngRoute', 'ngAnimate'])
-	// .config([
-  //       '$locationProvider',
-  //       '$routeProvider',
-  //       function($locationProvider, $routeProvider) {
-  //       	$locationProvider.hashPrefix('!');
-  //       	$routeProvider.when("/",{
-  //       		templateUrl: './views/partial1.html',
-  //       		controller: 'MainController'
-  //       	})
-  //       	.otherwise({
-  //       		redirectTo: '/'
-  //       	});
-  //       }
-	// ])
-	// .controller('MainController',['$scope',mainCtrl]);
+	'use strict';
+        require('angular');
+        require('angular-route');
+        require('angular-animate');
+        var mainCtrl = require('./controllers/mainctrl');
+	angular.module('SampleApp', ['ngRoute', 'ngAnimate'])
+	.config([
+        '$locationProvider',
+        '$routeProvider',
+        function($locationProvider, $routeProvider) {
+        	$locationProvider.hashPrefix('!');
+        	$routeProvider.when("/",{
+        		templateUrl: '../views/_partial.html',
+        		controller: 'MainController'
+        	})
+        	.otherwise({
+        		redirectTo: '/'
+        	});
+        }
+	])
+	.controller('MainController',['$scope',mainCtrl]);
 
 }());
